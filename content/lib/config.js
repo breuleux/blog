c66361980f8fa5dd504c1566f071c840dd4034cd
Olivier Breuleux <breuleux@gmail.com>
2016-01-05 23:32:46 -0500

    remove unneeded files

--- content/lib/config.js
+++
@@ -1,9 +0,0 @@
-System.config({
-  baseURL: "/blog/lib/",
-  defaultJSExtensions: true,
-  transpiler: "none",
-  paths: {
-    "github:*": "../../output/blog/lib/jspm_packages/github/*",
-    "npm:*": "../../output/blog/lib/jspm_packages/npm/*"
-  }
-});
